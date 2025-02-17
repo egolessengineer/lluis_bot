564ca98b7f0f99fe9bdf3c93cac13c0cb28a8d83
Unyielding & Durable <149814380+egolessengineer@users.noreply.github.com>
2025-02-18 04:36:11 +0700

    fix:bot command

--- src/telegram/Bot.ts
+++ src/telegram/Bot.ts
@@ -2,7 +2,9 @@ import { Bot } from "grammy";
 
 const bot = (token: string) => {
   const bot = new Bot(token);
+  bot.start().then(() => console.log("Bot launched"));
   listenToCommands(bot);
+  return bot;
 };
 
 export default bot;
